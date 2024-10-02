742fa09857d091d8692304128508dcf33d6d53c8
fuadhasen <fuadhas6634@gmail.com>
2024-10-02 16:48:00 +0530

    update task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,10 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((results) => {
-        return results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason,
-        }));
-    });
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
-
