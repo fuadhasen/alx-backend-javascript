df5c8a9abe73456a513c127ed220f08a7aaef10b
fuadhasen <fuadhas6634@gmail.com>
2024-10-03 12:58:18 +0530

    add task 2-hbtn_course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Length must be a number');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLen) {
+    if (typeof newLen !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLen;
+  }
+
+  set students(newStu) {
+    if (!Array.isArray(newStu)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStu;
+  }
+}
