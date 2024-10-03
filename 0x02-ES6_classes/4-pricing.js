db49b90055f81ddbd1d2a6b1a990d8af54ae42ad
fuadhasen <fuadhas6634@gmail.com>
2024-10-03 14:11:37 +0530

    add task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} ${`(${this._currency._code})`}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
