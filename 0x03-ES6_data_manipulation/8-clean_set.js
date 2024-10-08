146116535c3a108f0fb60c1ca3be2fb4e885f626
fuadhasen <fuadhas6634@gmail.com>
2024-10-08 16:25:02 +0530

    update task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,17 @@
 export default function cleanSet(set, startString) {
-  let str = '';
-
-  if (startString === '') {
+    let str = '';
+  
+    if (startString === '' || typeof (startString) !== 'string') {
+      return str;
+    }
+    for (const element of set) {
+      if (typeof (element) === 'string' && element.startsWith(startString)) {
+        if (str === '') {
+          str = str.concat(element.slice(startString.length));
+        } else {
+          str = str.concat(`-${element.slice(startString.length)}`);
+        }
+      }
+    }
     return str;
   }
-
-  set.forEach((item) => {
-    if (item.startsWith(startString)) {
-      str += `${item.slice(startString.length)}-`;
-    }
-  });
-  return str ? str.slice(0, -1) : str;
-}
